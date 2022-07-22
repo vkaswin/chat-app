98427e370827ef601ead72801f9b697e92d1d844
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2022-07-22 15:03:43 +0530

    Updates

--- static/js/77.5f2ae3d6.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_chat_app=self.webpackChunkreact_chat_app||[]).push([[77],{40077:function(t,n,a){a.r(n),a.d(n,{authURL:function(){return e},baseURL:function(){return c},endpoints:function(){return r}});var c="http://localhost:8000",e="/api/auth",r={auth:{login:"".concat(e,"/login"),register:"".concat(e,"/register")}}}}]);
-//# sourceMappingURL=77.5f2ae3d6.chunk.js.map
